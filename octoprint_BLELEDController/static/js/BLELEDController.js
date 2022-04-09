0366417363603a0d85081a89f26efd829a5aec3c
Luis Mercado <luisbzmercado1776@gmail.com>
2022-04-09 15:55:26 +0800

    add camera integration to tab view

--- octoprint_BLELEDController/static/js/BLELEDController.js
+++ octoprint_BLELEDController/static/js/BLELEDController.js
@@ -10,24 +10,27 @@ $(function() {
 
         // view models
         self.settingsViewModel = parameters[0];
+        self.controlViewModel = parameters[1];
 
         // binding variables
         self.color = ko.observable();
         self.is_on = ko.observable();
         self.brightness = ko.observable();
+        self.webcam_streamUrl = ko.computed(function(){
+			if(self.settingsViewModel.webcam_streamUrl() !== "") {
+				return self.settingsViewModel.webcam_streamUrl();
+			} else {
+				return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg==";
+			}
+		});
 
-        self.updateColor = function(picker, event) {
-            var newColor = event.currentTarget.jscolor.toHEXString()
-            if(newColor) {
-                self.color(newColor);
-            }
-        };
 
+        // hooks
         self.onBeforeBinding = function() {
             self.updateColorView(self.settingsViewModel.settings.plugins.BLELEDController.led_strip.hex_color());
             self.is_on(self.settingsViewModel.settings.plugins.BLELEDController.led_strip.is_on());
             self.brightness(self.settingsViewModel.settings.plugins.BLELEDController.led_strip.brightness());
-        }
+        };
 
         self.onDataUpdaterPluginMessage = function(plugin, data) {
             if(plugin != 'BLELEDController') { return }
@@ -39,13 +42,21 @@ $(function() {
                 if (data.led_strip.hasOwnProperty('brightness'))
                     self.brightness(data.led_strip.brightness);
             }
-        }
+        };
 
+        // custom-defined functions
         self.updateColorView = function(rcv_color_hex){
             var js_color_input = "".concat('#', rcv_color_hex.toString(16));
             self.color(js_color_input);
             document.querySelector('#color-picker-control').jscolor.fromString(self.color());
         }
+        
+        self.updateColor = function(picker, event) {
+            var newColor = event.currentTarget.jscolor.toHEXString()
+            if(newColor) {
+                self.color(newColor);
+            }
+        };
 
         self.applyChanges = function() {
             var color_hex = self.color().substring(1);
@@ -70,10 +81,11 @@ $(function() {
                 }
             });
         };
-    }
+        
+    };   
     OCTOPRINT_VIEWMODELS.push({
         construct: BleledcontrollerViewModel,
-        dependencies: [ "settingsViewModel" ],
+        dependencies: [ "settingsViewModel", "controlViewModel" ],
         elements: [ "#tab_plugin_BLELEDController" ]
     });
 });
